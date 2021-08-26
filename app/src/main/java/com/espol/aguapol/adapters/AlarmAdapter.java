520668e494652c8a06675bbbaced09c8a48959cf
ppereira123 <52977467+ppereira123@users.noreply.github.com>
2021-08-26 05:54:09 -0500

    Alarmas

--- app/src/main/java/com/espol/aguapol/adapters/AlarmAdapter.java
+++ app/src/main/java/com/espol/aguapol/adapters/AlarmAdapter.java
@@ -3,6 +3,9 @@ package com.espol.aguapol.adapters;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,6 +30,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -80,9 +84,26 @@ public class AlarmAdapter extends BaseAdapter {
         txtTitulo.setText(itemAlarma.getTitulo());
         txtFechaHora.setText(itemAlarma.getFechaHora());
         txtMensaje.setText(itemAlarma.getMensaje());
-        imgIcono.setImageResource(itemAlarma.getUrlIcon());
-
+        //imgIcono.setImageResource(itemAlarma.getUrlIcon());
+        Resources res = context.getResources();
+        String[] estadosAlarma = res.getStringArray(R.array.estadoAlarmas);
         CardView cardAlarma=convertView.findViewById(R.id.cardAlarma);
+        if(itemAlarma.getEstado()!=null){
+            if(itemAlarma.getEstado().equals(estadosAlarma[0])){
+                cardAlarma.setCardBackgroundColor(Color.parseColor("#00ACEE"));
+            }else if(itemAlarma.getEstado().equals(estadosAlarma[1])){
+                cardAlarma.setCardBackgroundColor(Color.parseColor("#00BB2D"));
+            }else if(itemAlarma.getEstado().equals(estadosAlarma[2])){
+                cardAlarma.setCardBackgroundColor(Color.parseColor("#FF0000"));
+            }else {
+                cardAlarma.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+            }
+
+        }else {
+            cardAlarma.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+        }
+
+
         View finalConvertView = convertView;
         cardAlarma.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -95,6 +116,8 @@ public class AlarmAdapter extends BaseAdapter {
                 builder.setPositiveButton("Solucionado", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        /*
+
                         DatabaseReference alarmasGuardadas= database.getReference("Alarmas solucionadas");
                         AlarmaManejada alarmaManejada= new AlarmaManejada(itemAlarma.getTitulo(),itemAlarma.getMensaje(),itemAlarma.getFechaHora(),itemAlarma.getUrlIcon(),itemAlarma.getId(),firebaseAuth.getCurrentUser().getEmail());
                         DatabaseReference refPush= alarmasGuardadas.push();
@@ -110,17 +133,41 @@ public class AlarmAdapter extends BaseAdapter {
                                 });
                             }
                         });
+
+                         */
+
+                        itemAlarma.setEstado(estadosAlarma[2]);
+                        DatabaseReference ref= database.getReference("Alarmas");
+                        ref.child(itemAlarma.getId()).setValue(itemAlarma);
+                        Intent intent=new Intent(context,AlarmaSolucionada.class);
+                        intent.putExtra("alarma", (Alarma) itemAlarma);
+                        context.startActivity(intent);
+
+
+                        dialog.dismiss();
+
+
+
+
+
+
                     }
                 });
                 builder.setNegativeButton("Cerrar", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        //vista cambiar a color azul
+                        cardAlarma.setCardBackgroundColor(Color.parseColor("#00ACEE"));
+                        itemAlarma.setEstado(estadosAlarma[0]);
+                        DatabaseReference ref= database.getReference("Alarmas");
+                        ref.child(itemAlarma.getId()).setValue(itemAlarma);
                         dialog.dismiss();
                     }
                 });
-                builder.setNeutralButton("Eliminar", new DialogInterface.OnClickListener() {
+                builder.setNeutralButton("En proceso", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        /*
                         DatabaseReference ref= database.getReference("Alarmas");
                         ref.child(itemAlarma.getId()).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
@@ -129,6 +176,13 @@ public class AlarmAdapter extends BaseAdapter {
                             }
                         });
                         dialog.dismiss();
+
+                         */
+                        cardAlarma.setCardBackgroundColor(Color.parseColor("#00BB2D"));
+                        itemAlarma.setEstado(estadosAlarma[1]);
+                        DatabaseReference ref= database.getReference("Alarmas");
+                        ref.child(itemAlarma.getId()).setValue(itemAlarma);
+
                     }
                 });
                 AlertDialog dialog= builder.create();
---
+++ app/src/main/java/com/espol/aguapol/adapters/AlarmaSolucionada.java
@@ -0,0 +1,16 @@
+package com.espol.aguapol.adapters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.espol.aguapol.R;
+
+public class AlarmaSolucionada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarma_solucionada);
+    }
+}
